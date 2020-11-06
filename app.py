aad7946ecb7adba61016c5f1449bbd791f869e57
Steven Carton <spatc@protonmail.com>
2020-11-06 02:34:30 -0500

    almost finished

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from models import connect_db, db, User
-from forms import RegisterForm, LoginForm
+from models import connect_db, db, User, Feedback
+from forms import RegisterForm, LoginForm, FeedbackForm
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgres:///feedback_db"
@@ -33,17 +33,69 @@ def show_registration():
         db.session.commit()
         session['username'] = new_user.username
         flash('Welcome! Successfully Created Your Account', "success")
-        return redirect('/secret')
+        return redirect(f'/users/{new_user.username}')
     return render_template('register.html', form=form)
 
-@app.route('/secret')
-def show_secret():
-    return render_template('secret.html')
+@app.route('/users/<username>')
+def show_secret(username):
+    if "username" not in session:
+        flash("Please login first!", "danger")
+        return redirect('/')
+    user = User.query.get(username)
+    feedbacks = Feedback.query.filter_by(username=username).all()
+    return render_template('secret.html', user=user, feedbacks=feedbacks)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login_user():
     form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        user = User.authenticate(username, password)
+        if user:
+            flash(f"Welcome Back, {user.username}", "primary")
+            session['username'] = user.username
+            return redirect(f'/users/{user.username}')
+        else:
+            form.username.errors = ['Invalid username/password.']
     return render_template('login.html', form=form)
 
+@app.route('/logout')
+def logout_user():
+    session.pop('username')
+    flash('Goodbye', "info")
+    return redirect('/')
 
+@app.route('/users/<username>/feedback/add', methods=["POST", "GET"])
+def add_feedback(username):
+    if 'username' not in session:
+        flash("Please login first!", "danger")
+        return redirect('/')
+    form = FeedbackForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.content.data
+        fb = Feedback(title=title,content=content,username=session["username"])
+        db.session.add(fb)
+        db.session.commit()
+        flash('Feedback Added!', "success")
+        return redirect(f'/users/{username}')
+    return render_template('add_feedback.html', form=form)
 
+@app.route('/feedback/<int:id>/update', methods=["POST", "GET"])
+def update_feedback(id):
+        if 'username' not in session:
+            flash("Please login first!", "danger")
+            return redirect('/')
+        fb = Feedback.query.get_or_404(id)
+        form = FeedbackForm(obj=fb)
+
+        if form.validate_on_submit():
+            fb.title = form.title.data
+            fb.content = form.content.data   
+            db.session.commit()
+            flash('Feedback Updated', "primary")
+            return redirect(f"/users/{fb.user.username}")
+        else:
+            return render_template("add_feedback.html", form=form)
